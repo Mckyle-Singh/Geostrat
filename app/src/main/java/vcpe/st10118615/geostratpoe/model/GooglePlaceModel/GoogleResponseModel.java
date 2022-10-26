a948d3cb8544aa65c358e875c304fa2828b57825
Denzel <email@denzel>
2022-10-27 01:23:36 +0200

    add google place model classes

---
+++ app/src/main/java/vcpe/st10118615/geostratpoe/model/GooglePlaceModel/GoogleResponseModel.java
@@ -0,0 +1,34 @@
+package vcpe.st10118615.geostratpoe.model.GooglePlaceModel;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+import vcpe.st10118615.geostratpoe.GooglePlaceModel;
+
+public class GoogleResponseModel {
+    @SerializedName("results")
+    @Expose
+    private List<GooglePlaceModel> googlePlaceModelList;
+
+    @SerializedName("error_message")
+    @Expose
+    private String error;
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public List<GooglePlaceModel> getGooglePlaceModelList() {
+        return googlePlaceModelList;
+    }
+
+    public void setGooglePlaceModelList(List<GooglePlaceModel> googlePlaceModelList) {
+        this.googlePlaceModelList = googlePlaceModelList;
+    }
+}
