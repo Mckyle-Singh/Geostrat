f347a8a72a1772aca6d114915565ab58a58f09be
Denzel <email@denzel>
2022-10-27 01:28:02 +0200

    add util and web service classes

---
+++ app/src/main/java/vcpe/st10118615/geostratpoe/utility/LoadingDialog.java
@@ -0,0 +1,4 @@
+package vcpe.st10118615.geostratpoe.utility;
+
+public class LoadingDialog {
+}
---
+++ app/src/main/java/vcpe/st10118615/geostratpoe/webservices/RetrofitAPI.java
@@ -0,0 +1,15 @@
+package vcpe.st10118615.geostratpoe.webservices;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Url;
+import vcpe.st10118615.geostratpoe.model.DirectionPlaceModel.DirectionResponseModel;
+import vcpe.st10118615.geostratpoe.model.GooglePlaceModel.GoogleResponseModel;
+
+public interface RetrofitAPI {
+    @GET
+    Call<GoogleResponseModel> getNearByPlaces(@Url String url);
+
+    @GET
+    Call<DirectionResponseModel> getDirection(@Url String url);
+}
